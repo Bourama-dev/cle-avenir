2f1b422c8d8e2752d3bf436d42ffb2af7d9c8a50
Bourama-dev <bouramad900@gmail.com>
2026-04-07 19:19:05 +0200

    chore: suppression de 28 fichiers pages orphelins et doublons

--- src/pages/AdminDashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AdminDashboardComponent from '@/components/AdminDashboard';
-
-const AdminDashboard = (props) => {
-  return <AdminDashboardComponent {...props} />;
-};
-
-export default AdminDashboard;
